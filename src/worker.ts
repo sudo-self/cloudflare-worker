14bfe4899380cece6ad0575f7668d3a403dc0717
CodeSandbox <support@codesandbox.io>
2023-10-31 11:00:20 +0000

    Initial commit

--- src/worker.ts
+++ src/worker.ts
@@ -27,6 +27,6 @@ export interface Env {
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello CodeSandbox!');
+		return new Response('Hello JesseJesse.com!');
 	},
 };
